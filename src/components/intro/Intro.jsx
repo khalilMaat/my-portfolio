08ea536d0ccd216ad44a7f5acfdb2abf4d7fd05b
khalilMaat <maatoug620@outlook.fr>
2024-03-09 16:14:24 +0100

    my portfolio

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,39 @@
+import './intro.scss';
+import {init} from "ityped";
+import React, {useEffect, useRef} from "react";
+export default function Intro(){
+    const textRef = useRef();
+
+    useEffect(()=>{
+        //console.log(textRef)
+        
+        init(textRef.current,{
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed:50,
+            strings:["DevOps Engineer","Web Developer","Game Developer","Designer","Freelancer"],
+        });
+    },[]);
+    return(
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/me.png" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Maatoug Khalil</h1>
+                    <h3><span ref={textRef}></span></h3>
+                </div>
+               
+                <a href="#portfolio">
+                    <img src="assets/mous.png" alt="" />
+                    
+                </a>
+                
+            </div>
+        </div>
+    )
+}
